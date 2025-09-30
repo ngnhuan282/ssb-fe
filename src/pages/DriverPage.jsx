05031ef1e058ae15cfb5a8e01e88ccc350d538b8
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-09-30 22:30:53 +0700

    init basic page

---
+++ src/pages/DriverPage.jsx
@@ -0,0 +1,7 @@
+const DriverPage = () => {
+    return (
+        <div>Đây là trang quản lý tài xế</div>
+    );
+}
+
+export default DriverPage;
---
+++ src/pages/UserPage.jsx
@@ -0,0 +1,7 @@
+const UserPage = () => {
+    return (
+        <div>Đây là trang quản lý người dùng</div>
+    );
+}
+
+export default UserPage;
