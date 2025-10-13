bcff8521ee1baa9b68e60d2ec97f77726b1d666f
ngnhuan282 <nguyennhuan2003@gmail.com>
2025-10-13 22:48:20 +0700

    up

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -9,7 +9,6 @@ import DriverPage from "../pages/user/DriverPage";
 import LoginPage from "../pages/user/LoginPage";
 import RegisterPage from "../pages/user/RegisterPage";
 import ErrorPage from "../pages/user/ErrorPage";
-import { LoginPageStyle1, LoginPageStyle2, LoginPageStyle3 } from "../pages/user/LoginPageStyle";
 
 // ADMIN pages
 import AdminDashboard from "../pages/admin/AdminDashboard";
@@ -28,9 +27,6 @@ export const router = createBrowserRouter([
     ],
   },
   { path: "/login", element: <LoginPage /> },
-  { path: "/login1", element: <LoginPageStyle1 /> },
-  { path: "/login2", element: <LoginPageStyle2 /> },
-  { path: "/login3", element: <LoginPageStyle3 /> },
   { path: "/register", element: <RegisterPage /> },
   { path: "/admin", element: <AdminDashboard /> },
 ]);
